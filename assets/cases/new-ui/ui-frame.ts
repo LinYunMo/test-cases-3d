166d63f25c96f172c7cd0068e21a1ca99735d7fb
ChiaNing <sun.clooud.mo@gmail.com>
2022-11-09 14:58:49 +0800

    update

--- assets/cases/new-ui/ui-frame.ts
+++ assets/cases/new-ui/ui-frame.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, UISystem, UIDocument, UIElement, Rect, Color } from 'cc';
+import { _decorator, Component, Node, UISystem, UIDocument, UIElement, Rect, Color, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('ui_frame')
@@ -6,6 +6,7 @@ export class ui_frame extends Component {
 
     uiDoc = new UIDocument();
     uiElement = new UIElement();
+    uiE2 = new UIElement();
 
     onload () {
         
@@ -13,14 +14,22 @@ export class ui_frame extends Component {
 
     start() {
         UISystem.instance.addDocument(this.uiDoc);
-        let rect =  this.uiElement.layout = new Rect(0, 0, 100, 100);
+        let rect =  this.uiElement.layout = new Rect(50, 50, 100, 100);
+        // this.uiElement.position = new Vec3(100, 100, 0);
         let color = Color.WHITE;
+        this.uiDoc.window.addChild(this.uiElement);
+        this.uiDoc.renderSubSystem.context.paint(this.uiElement);
         this.uiDoc.renderSubSystem.context.drawRect(rect, color);
+
+        // rect =  this.uiE2.layout = new Rect(-50, -50, 200, 200);
+        // color = Color.BLUE;
+        // this.uiDoc.window.addChild(this.uiE2);
+        // this.uiDoc.renderSubSystem.context.paint(this.uiE2);
+        // this.uiDoc.renderSubSystem.context.drawRect(rect, color);
     }
 
     update(deltaTime: number) {
         
     }
 }
-
 
